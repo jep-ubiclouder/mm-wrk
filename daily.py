79dde0092e0c9f889f5d9d7729f3e50090a28aec
jep-ubiclouder <jean-eric.preis@ubiclouder.com>
2017-06-09 09:47:51 +0200

    mapfileds loaded

--- daily.py
+++ daily.py
@@ -11,11 +11,16 @@ print(compte['records'])
 
 import os.path
 import csv
-
+mapFields = {}
 mapfile = open('./bucket-mm-daily/map-lignes.sdl','r')
 for l in mapfile.readlines():
-    
-    
+    if l[0] =='#':
+        continue
+    (clefSTX,clefSF) = l.split('=')
+    mapFields[clefSTX]=clefSF
+
+print mapFields
+
 
 with open('./bucket-mm-daily/lo-2017-test.csv', 'r') as csvfile:
         reader=  csv.reader(csvfile,delimiter=';')
