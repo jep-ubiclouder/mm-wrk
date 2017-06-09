154c8b8329803abd67777d50834b8afc456b94a0
jep-ubiclouder <jean-eric.preis@ubiclouder.com>
2017-06-09 17:02:44 +0200

    rzaqftz

--- daily.py
+++ daily.py
@@ -41,8 +41,8 @@ with open('./bucket-mm-daily/lo-2017-test.csv', 'r') as csvfile:
                     value= '%s-%s-%s'%(a,m,d)
                     row[clef]=value
                  
-                    record[mapFields[clef]]=row[clef].decode('utf-8')
-                    print clef, row[clef],record[mapFields[clef]]
+                record[mapFields[clef]]=row[clef].decode('utf-8')
+                print clef, row[clef],record[mapFields[clef]]
                
             """try:
                 
