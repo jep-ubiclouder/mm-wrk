7c8cee399b6d36496a1d5007131d29139b5cb546
jep-ubiclouder <jean-eric.preis@ubiclouder.com>
2017-06-09 14:20:38 +0200

    mass delete

--- daily.py
+++ daily.py
@@ -6,8 +6,18 @@ Created on 8 juin 2017
 from simple_salesforce import Salesforce
 sf = Salesforce(username='jep@assembdev.com', password='ubi$2017', security_token='aMddugz7oc45l1uhqWAE308Z', sandbox=True)
 
-compte = sf.query("Select  id,name,Cle_Client_STOCKX__c,BillingCity from Account  where Cle_Client_STOCKX__c = 'C1037'")
-print(compte['records'])
+#compte = sf.query("Select  id,name,Cle_Client_STOCKX__c,BillingCity from Account  where Cle_Client_STOCKX__c = 'C1037'")
+#print(compte['records'])
+
+toto = sf.query('select id from Lignes_commande__c')
+for r in toto['records']:
+    id=r['Id']
+    sf.Lignes_commande__c.delete(id)
+while toto.hasKey['nextRecordUrl']:
+    toto = sf.query_more(toto.hasKey['nextRecordUrl'])
+    for r in toto['records']:
+        id=r['Id']
+        sf.Lignes_commande__c.delete(id)
 
 import os.path
 import csv
@@ -23,11 +33,11 @@ print( mapFields)
 
 with open('./bucket-mm-daily/lo-2017-test.csv', 'r') as csvfile:
         reader=  reader = csv.DictReader(csvfile,delimiter=';')
-        for row in reader:
+        for row in reader[:20]:
             record={}
             for clef in row.keys():
                 # print row
-                print clef, row[clef]
+                ## print clef, row[clef]
                 if clef=='DATE_CDE' or clef == 'PARUTION':
                     (d,m,a) = row[clef].split('/')
                     value= '%s-%s-%s'%(a,m,d)
