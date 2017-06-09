44095447be11688c8a4534c9e9148ec13df43954
jep-ubiclouder <jean-eric.preis@ubiclouder.com>
2017-06-09 10:13:11 +0200

    testing on sandbox

--- daily.py
+++ daily.py
@@ -4,7 +4,7 @@ Created on 8 juin 2017
 @author: jean-eric preis
 '''
 from simple_salesforce import Salesforce
-sf = Salesforce(username='jep@ubitask2.com', password='Ubi$2017', security_token='bnWf5zUKZg8hPjPP5Qein2HW')
+sf = Salesforce(username='jep@assembdev.com', password='ubi$2017', security_token='aMddugz7oc45l1uhqWAE308Z', sandbox=True)
 
 compte = sf.query("Select  id,name from Account  where AccountNumber = 'CD656092'")
 print(compte['records'])
@@ -21,13 +21,13 @@ for l in mapfile.readlines():
 
 print mapFields
 
-
-
-
 with open('./bucket-mm-daily/lo-2017-test.csv', 'r') as csvfile:
         reader=  reader = csv.DictReader(csvfile,delimiter=';')
         for row in reader:
-            print row
-
+            record={}
+            for (clef,value) in row:
+                record[mapFields[clef]]=value
+            reponse = sf.Ligne_order__c.create(record)
+            
 if __name__ == '__main__':
     pass
