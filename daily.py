d82cbf3b760a739b4b173d17a3a3d1f3bc39baac
jep-ubiclouder <jean-eric.preis@ubiclouder.com>
2017-06-20 15:20:10 +0200

    parsearg

--- daily.py
+++ daily.py
@@ -46,70 +46,58 @@ def findFile(parmDate=None):
     base ='./bucket-mm-daily/EXPORT_%s.CSV'%compactDate
     return base
     
-
-sf = Salesforce(username='jep@assembdev.com', password='ubi$2017', security_token='aMddugz7oc45l1uhqWAE308Z', sandbox=True)
-
-## toto = sf.query('select id from Lignes_commande__c')
-""" for r in toto['records']:
-    id=r['Id']
-    print( 'deleting ',id)
-    sf.Lignes_commande__c.delete(id)
-while 'nextRecordUrl' in toto.keys():
-    toto = sf.query_more(toto['nextRecordUrl'])
-    for r in toto['records']:
-        id=r['Id']
-        print( 'deleting ',id)
-        sf.Lignes_commande__c.delete(id) """
-import os.path
-import csv
-mapFields = {}
-mapfile = open('./bucket-mm-daily/mapping_lignes-de-commandes2017.sdl','r')
-for l in mapfile.readlines():
-    if l[0] =='#':
-        continue
-    (clefSTX,clefSF) = l.split('=')
-    mapFields[clefSTX]=clefSF[:-1]
-print( mapFields)
-i=0
-updates ={}
-insertions ={}
-
-with open(findFile(), 'r',encoding='utf-8') as csvfile:
-    reader=  csv.DictReader(csvfile,delimiter=',')
-    for row in reader:
-        record={}
-        statut = row['STATUT'] 
-        commande=row['COMMANDE']
-        if commande=='123456':continue
-        Index_STOCKX__c = row['IND']
-        for clef in row.keys():
-            if clef =='IND': 
-                continue 
-                ## Nous faisons des upsert le champs Index_STOCKX__c NE DOIT PAS apparraitre dans le record
-            if clef in mapFields.keys():
-                ## passage en AA-MM-JJ
-                if clef=='DATE_CDE' or clef == 'PARUTION':
-                    print(clef, row[clef])                    
-                    (d,m,a) = row[clef].split('-')
-                    value= '%s-%s-%s'%(a,m,d)
-                    row[clef]=value
-                try: 
-                    record[mapFields[clef]]=row[clef]
-                except :
-                    print('ooops')
-        try:
-            insertions[Index_STOCKX__c] = record
-            pass
-        except :
-           continue
-        i += 1
-
-    for clef in insertions.keys() :
-        reponse = sf.Lignes_commande__c.upsert('Index_STOCKX__c/%s'%clef,insertions[clef], raw_response=True)
-        print(reponse.text)
-
-    print(dir(reponse))   
-    sendmail(insertions)
+def process(parmDate):
+    sf = Salesforce(username='jep@assembdev.com', password='ubi$2017', security_token='aMddugz7oc45l1uhqWAE308Z', sandbox=True)
+    import os.path
+    import csv
+    mapFields = {}
+    mapfile = open('./bucket-mm-daily/mapping_lignes-de-commandes2017.sdl','r')
+    for l in mapfile.readlines():
+        if l[0] =='#':
+            continue
+        (clefSTX,clefSF) = l.split('=')
+        mapFields[clefSTX]=clefSF[:-1]
+    print( mapFields)
+    i=0
+    updates ={}
+    insertions ={}
+    
+    with open(findFile(), 'r',encoding='utf-8') as csvfile:
+        reader=  csv.DictReader(csvfile,delimiter=',')
+        for row in reader:
+            record={}
+            statut = row['STATUT'] 
+            commande=row['COMMANDE']
+            if commande=='123456':continue
+            Index_STOCKX__c = row['IND']
+            for clef in row.keys():
+                if clef =='IND': 
+                    continue 
+                    ## Nous faisons des upsert le champs Index_STOCKX__c NE DOIT PAS apparraitre dans le record
+                if clef in mapFields.keys():
+                    ## passage en AA-MM-JJ
+                    if clef=='DATE_CDE' or clef == 'PARUTION':
+                        print(clef, row[clef])                    
+                        (d,m,a) = row[clef].split('-')
+                        value= '%s-%s-%s'%(a,m,d)
+                        row[clef]=value
+                    try: 
+                        record[mapFields[clef]]=row[clef]
+                    except :
+                        print('ooops')
+            try:
+                insertions[Index_STOCKX__c] = record
+                pass
+            except :
+               continue
+            i += 1
+    
+        for clef in insertions.keys() :
+            reponse = sf.Lignes_commande__c.upsert('Index_STOCKX__c/%s'%clef,insertions[clef], raw_response=True)
+            print(reponse.text)
+    
+        print(dir(reponse))   
+        sendmail(insertions)
     
 if __name__ == '__main__':
     import argparse
