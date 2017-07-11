3dcfa83fbe2e9c1b7a48014f313be0c2f3beb47c
jep-ubiclouder <jean-eric.preis@ubiclouder.com>
2017-07-11 10:10:48 +0200

    hello

---
+++ eurodep.py
@@ -0,0 +1,13 @@
+#!/usr/local/bin/python3.6
+#-*- coding: utf-8 -*-
+
+'''
+Created on 8 juin 2017
+
+@author: jean-eric preis
+'''
+from simple_salesforce import Salesforce
+import sys
+from _datetime import timedelta
+from datetime import date
+from ftplib import FTP_PORT
