589860e36ca49858cd946709e803bffa7d77e637
jep-ubiclouder <jean-eric.preis@ubiclouder.com>
2017-07-11 10:46:40 +0200

    parametre date

--- eurodep.py
+++ eurodep.py
@@ -12,9 +12,29 @@ from _datetime import timedelta
 from datetime import date
 from ftplib import FTP
 
-eurodep= FTP(host='ftp.eurodep.fr',user='HOMMEDEFER',passwd='lhdf515')
 
-#truc =  eurodep.retrlines('LIST')
-truc = eurodep.nlst('*.CSV')
-for t in truc:
-    eurodep.retrbinary('RETR %s'%t, open('%s'%t,'wb').write)
+
+def getfromFTP(compactDate,now):
+    print(compactDate)
+    eurodep= FTP(host='ftp.eurodep.fr',user='HOMMEDEFER',passwd='lhdf515')    
+    
+    truc = eurodep.nlst('*%s.csv'%compactDate)
+    for t in truc:
+        eurodep.retrbinary('RETR %s'%t, open('%s'%t,'wb').write)
+    return truc[0]
+   
+if __name__ == '__main__':
+    import argparse
+    
+    parser = argparse.ArgumentParser(description='Short sample app')
+    parser.add_argument('-d','--date', action="store", dest="parmDate")
+    
+    args = parser.parse_args()
+    from datetime import datetime, timedelta
+    if args.parmDate :
+        now = datetime.strptime(args.parmDate, '%Y-%m-%d')
+    if args.parmDate is None:
+        now = datetime.now() -timedelta(days=1)
+    compactDate='%s%02i%02i'%(now.year,now.month,now.day)
+    fn = getfromFTP(compactDate,now)
+    print(fn) 
