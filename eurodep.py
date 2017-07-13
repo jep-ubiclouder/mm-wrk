9ad25a330b0bd3bce46841392301e2dfffb46c47
jep-ubiclouder <jean-eric.preis@ubiclouder.com>
2017-07-13 11:20:01 +0200

    quoted

--- eurodep.py
+++ eurodep.py
@@ -51,7 +51,7 @@ def processFile(fname):
                codes_cli.append("%s"%row['CODCLI'])
         
     print(codes_cli) 
-    res = 'select id,name from account where Code_EURODEP__c in ('+','.join(codes_cli)+')'
+    res = 'select id,name from account where Code_EURODEP__c in ('+','.join(["\'%s\'"%c for c in codes_cli])+')'
     print(res)
     les_ids =sf.query(res)
     print(les_ids)
