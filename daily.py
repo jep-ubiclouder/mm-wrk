5679b6508a8fd8eb2bff038d489b6d45f5797651
jep-ubiclouder <jean-eric.preis@ubiclouder.com>
2017-07-17 17:35:57 +0200

    insertion = false

--- daily.py
+++ daily.py
@@ -145,7 +145,7 @@ def process(parmDate, now):
     with open(findFile(parmDate), 'r', encoding='utf-8') as csvfile:
         reader = csv.DictReader(csvfile, delimiter=',')
         for row in reader:
-            inserer = True
+            inserer = False
             record = {}
             statut = row['STATUT']
             commande = row['COMMANDE']
