ae006cf10c749e33179c444b1bedd34797e0585c
jep-ubiclouder <jean-eric.preis@ubiclouder.com>
2017-09-01 16:30:51 +0200

    encrypt

--- pickling.py
+++ pickling.py
@@ -1,8 +1,11 @@
+from cryptography.fernet import Fernet
+clef =b'y1RrSzZel5RRjMjCZwwLVnVppKzqHQT0v-Mm96WNdS4='
+
 import json
 
 credentials ={'user':'jep@assembdev.com', 'passwd':'ubi$2017', 'security_token':'aMddugz7oc45l1uhqWAE308Z'}
 
 strCreds =  json.dumps(credentials)
 print(strCreds)
-
-    
+cipher =  Fernet(clef)
+print(cipher.encrypt(strCreds))
