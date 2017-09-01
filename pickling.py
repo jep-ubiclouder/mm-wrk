087e91d1ae4f716a6d2be54ad9ea82dfeed1a4b1
jep-ubiclouder <jean-eric.preis@ubiclouder.com>
2017-09-01 16:27:58 +0200

    plop

--- pickling.py
+++ pickling.py
@@ -2,7 +2,7 @@ import json
 
 credentials ={'user':'jep@assembdev.com', 'passwd':'ubi$2017', 'security_token':'aMddugz7oc45l1uhqWAE308Z'}
 
-strCreds = c json.dumps(credentials)
+strCreds =  json.dumps(credentials)
 print strCreds
 
     
