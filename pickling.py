b7ca6dd327370abbf8a3816e97f54f54689f73c1
jep-ubiclouder <jean-eric.preis@ubiclouder.com>
2017-09-06 14:45:32 +0200

    zer

--- pickling.py
+++ pickling.py
@@ -2,10 +2,6 @@ from cryptography.fernet import Fernet
 clef =b'y1RrSzZel5RRjMjCZwwLVnVppKzqHQT0v-Mm96WNdS4='
 data =b'gAAAAABZqXG7ztOFfBzQyWInJBGi2ug_TnFsZFLM0fbCCx6POD8ki_qCJRIlIm8jBh9Z918JxLi3He-46-rcoIZ_BgrwD9VvdYRA5_6G5k8FySU0m7qpPnV6UDh6ayqPlR-mvo8Dp3DTi8xElZVhp2tKtZBL95tl-5e2XtNsz67D1lkHui856v2G5jTh6zYNMS3ifjvnl1DQ0SuEKZ2FZay031QRu1q7Wg=='
 import json
-
-
-#strCreds =  json.dumps(credentials)
-## print(strCreds)
 cipher =  Fernet(clef)
 creds = json.loads(cipher.decrypt(data).decode())
 print(creds['user'])
