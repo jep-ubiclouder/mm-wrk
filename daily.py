432598c243722d6b4bd69a38f00d38b776ef8d0a
jep-ubiclouder <jean-eric.preis@ubiclouder.com>
2017-09-08 11:18:55 +0200

    args

--- daily.py
+++ daily.py
@@ -306,7 +306,7 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='Short sample app')
     parser.add_argument('-d', '--date', action="store", dest="parmDate")
-    parser.add_argument('-t', '--test',default=False)
+    parser.add_argument('-t', '--test',default=False,nargs=?)
     args = parser.parse_args()
     from datetime import datetime, timedelta
     if args.parmDate:
