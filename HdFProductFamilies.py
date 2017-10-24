b51f16873266a4be1a3d30e857eda3b8805ded4f
jep-ubiclouder <jep-ubiclouder@users.noreply.github.com>
2017-10-24 10:56:21 +0200

    Create HdFProductFamilies.py

---
+++ HdFProductFamilies.py
@@ -0,0 +1,25 @@
+#!/usr/local/bin/python3.6
+#-*- coding: utf-8 -*-
+
+'''
+Created on 24/10/2017
+
+@author: jean-eric preis
+'''
+
+from simple_salesforce import (
+    Salesforce,
+    SalesforceAPI,
+    SFType,
+    SalesforceError,
+    SalesforceMoreThanOneRecord,
+    SalesforceExpiredSession,
+    SalesforceRefusedRequest,
+    SalesforceResourceNotFound,
+    SalesforceGeneralError,
+    SalesforceMalformedRequest
+)
+
+import sys
+from _datetime import timedelta
+from datetime import date
