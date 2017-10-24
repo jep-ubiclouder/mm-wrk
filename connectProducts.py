02641796136913e61aa6affff5575458ea1131f2
jep-ubiclouder <jean-eric.preis@ubiclouder.com>
2017-10-24 16:56:41 +0200

    work1

--- connectProducts.py
+++ connectProducts.py
@@ -16,6 +16,21 @@ from _datetime import timedelta
 from datetime import date
 
 def processFile():
+    sf = Salesforce(username='projets@homme-de-fer.com', password='ubiclouder$2017', security_token='mQ8aTUVjtfoghbJSsZFhQqzJk')
+    qry =  'select id,Code_EAN_EURODEP__c from Commande__c where Code_EAN_EURODEP__c != null'
+    allLignes = sf.query_all(qry)
+    
+    allMissing = []
+    recs = allLignes['records']
+    for r in recs:
+        if r['Code_EAN_EURODEP__c'] not in allMissing:
+            allMissing.append(r['Code_EAN_EURODEP__c'])
+    print(allMissing)
+    
+    qrygetProducts = 'select id,EAN__c from Products2 where EAN__C in ( \'PLACEHOLDER\',' + ','.join(["\'%s\'" % c for c in allMissing]) + ')'
+    print(qrygetProducts)
+    allProductId = sf.query(qrygetProducts)
+    print(allProductId('records'))
   pass
 if __name__ == '__main__':
     processFile()
