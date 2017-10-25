0261e9a2a88c1799ca7d71229b5c2ce2fcbb50a3
jep-ubiclouder <jep-ubiclouder@users.noreply.github.com>
2017-10-25 09:02:40 +0200

    Create HdfStockquotidien.py

---
+++ HdfStockquotidien.py
@@ -0,0 +1,23 @@
+from simple_salesforce import (
+    Salesforce,
+    SalesforceAPI,
+    SFType,
+    SalesforceError,
+    SalesforceMoreThanOneRecord,
+    SalesforceExpiredSession,
+    SalesforceRefusedRequest,
+    SalesforceResourceNotFound,
+    SalesforceGeneralError,
+    SalesforceMalformedRequest
+)
+
+import sys
+from _datetime import timedelta
+from datetime import date
+from ftplib import FTP, all_errors
+
+import codecs
+import os.path
+import csv
+
+import pprint
