6beae8d45c05bf5f4dfbfa3cf5c99ec457e5f88b
jep-ubiclouder <jean-eric.preis@ubiclouder.com>
2017-10-25 09:24:45 +0200

    wrk1

--- HdfStockquotidien.py
+++ HdfStockquotidien.py
@@ -21,3 +21,24 @@ import os.path
 import csv
 
 import pprint
+
+def getfromFTP(compactDate):
+    """ 
+    Telecharge le fichier du jour de la date passée en parammètre format YYYYMMDD
+    Renvoie le nom du fichier ecrit sur le disque ou False si une erreur est survenue
+    """
+    eurodep = FTP(host='ftp.eurodep.fr', user='HOMMEDEFER', passwd='lhdf515')
+    try:
+        truc = eurodep.nlst('/OUT/IMG/OIMG515%s*.CSV.csv' % compactDate) #ftp://HOMMEDEFER@ftp.eurodep.fr/OUT/IMG/OIMG51517102400001.CSV
+    except all_errors as e:
+        print('No File today')
+        return False
+
+    for t in truc:
+        eurodep.retrbinary('RETR %s' % t, open('%s' % t, 'wb').write)
+    return truc[0]
+
+if __name__ == '__main__':
+    now =datetime.now() - timedelta(days=1)
+    compactDate = '%02i%02i%02i' % (now.year-2000, now.month, now.day)
+    print(compactDate)
