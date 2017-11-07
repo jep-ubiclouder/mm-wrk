a8e7469cccef44f5f19b3a200667294664182f71
jep-ubiclouder <jean-eric.preis@ubiclouder.com>
2017-11-07 10:52:59 +0100

    test 1

--- rechercheLigneSansCompte.py
+++ rechercheLigneSansCompte.py
@@ -22,13 +22,16 @@ from simple_salesforce import (
 import sys
 from _datetime import timedelta
 from datetime import date
-
+import csv
 def process():
     sf = Salesforce(username='projets@homme-de-fer.com', password='ubiclouder$2017', security_token='mQ8aTUVjtfoghbJSsZFhQqzJk')
     qry  ='SELECT Bon_de_livraison__c,Code_Client_EURODEP__c,Code_EAN_EURODEP__c,Compte__c,C_A_Brut__c,C_A_Net__c,Date_de_commande__c,Facture__c,Ligne__c,Prix_Brut__c,Prix_Net__c,Produit__c,Quantite__c,Reference_Client__c FROM Commande__c where compte__c = null and Code_Client_EURODEP__c = null  order by Date_de_commande__c desc'
-    result = sf.query_all(qry)['records']
+    ## result = sf.query_all(qry)['records']
     
-    
-    
+    with open('./histoventes.csv','w') as f:
+        w= csv.reader(f,delimiter='')
+        for l in w.readlines():
+            pass
+        print l
 if __name__ == '__main__':
     process()
