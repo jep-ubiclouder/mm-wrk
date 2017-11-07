4f25b0357e653183084a9b7dd906a3a69776baa7
jep-ubiclouder <jean-eric.preis@ubiclouder.com>
2017-11-07 10:36:08 +0100

    hello

--- rechercheLigneSansCompte.py
+++ rechercheLigneSansCompte.py
@@ -1,1 +1,34 @@
+#!/usr/local/bin/python3.6
+#-*- coding: utf-8 -*-
+
+'''
+Created on 07 Novembre 2017
+
+@author: jean-eric preis
+'''
+from simple_salesforce import (
+    Salesforce,
+    SalesforceAPI,
+    SFType,
+    SalesforceError,
+    SalesforceMoreThanOneRecord,
+    SalesforceExpiredSession,
+    SalesforceRefusedRequest,
+    SalesforceResourceNotFound,
+    SalesforceGeneralError,
+    SalesforceMalformedRequest
+)
+
 import sys
+from _datetime import timedelta
+from datetime import date
+
+def process():
+    sf = Salesforce(username='projets@homme-de-fer.com', password='ubiclouder$2017', security_token='mQ8aTUVjtfoghbJSsZFhQqzJk')
+    qry  ='SELECT Bon_de_livraison__c,Code_Client_EURODEP__c,Code_EAN_EURODEP__c,Compte__c,C_A_Brut__c,C_A_Net__c,Date_de_commande__c,Facture__c,Ligne__c,Prix_Brut__c,Prix_Net__c,Produit__c,Quantite__c,Reference_Client__c FROM Commande__c where compte__c = null and Code_Client_EURODEP__c = null  order by Date_de_commande__c desc'
+    result = sf.query_all(qry)['records']
+    
+    
+    
+if __name__ == '__main__':
+    process()
