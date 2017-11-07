6b0921e32cc0b22816fa17bab985139e24e1d09d
jep-ubiclouder <jean-eric.preis@ubiclouder.com>
2017-11-07 13:06:59 +0100

    debug

--- rechercheLigneSansCompte.py
+++ rechercheLigneSansCompte.py
@@ -26,7 +26,7 @@ import csv
 def process():
     sf = Salesforce(username='projets@homme-de-fer.com', password='ubiclouder$2017', security_token='mQ8aTUVjtfoghbJSsZFhQqzJk')
     qry  ='SELECT id,Bon_de_livraison__c,Code_Client_EURODEP__c,Code_EAN_EURODEP__c,Compte__c,C_A_Brut__c,C_A_Net__c,Date_de_commande__c,Facture__c,Ligne__c,Prix_Brut__c,Prix_Net__c,Produit__c,Quantite__c,Reference_Client__c FROM Commande__c where compte__c = null and Code_Client_EURODEP__c = null  order by Date_de_commande__c desc'
-    """ result = sf.query_all(qry)['records']
+    result = sf.query_all(qry)['records']
     unknownCompteByFacture = {} 
     
     
@@ -36,7 +36,7 @@ def process():
             unknownCompteByFacture[r['Facture__c']] = []
         
         unknownCompteByFacture[r['Facture__c']].append(r['Id'])
-    """    
+      
     allSorifa =[]
     byFacture ={}
     # je relie les factures avec les code SORIFA
