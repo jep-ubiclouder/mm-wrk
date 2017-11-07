c322237ee17aa62621c5a21bb14081e5d2e7827d
jep-ubiclouder <jean-eric.preis@ubiclouder.com>
2017-11-07 11:05:49 +0100

    file

--- rechercheLigneSansCompte.py
+++ rechercheLigneSansCompte.py
@@ -28,7 +28,7 @@ def process():
     qry  ='SELECT Bon_de_livraison__c,Code_Client_EURODEP__c,Code_EAN_EURODEP__c,Compte__c,C_A_Brut__c,C_A_Net__c,Date_de_commande__c,Facture__c,Ligne__c,Prix_Brut__c,Prix_Net__c,Produit__c,Quantite__c,Reference_Client__c FROM Commande__c where compte__c = null and Code_Client_EURODEP__c = null  order by Date_de_commande__c desc'
     ## result = sf.query_all(qry)['records']
     
-    with open('./histoventes.csv','r') as f:
+    with open('./venteshisto.csv','r') as f:
         
         reader = csv.DictReader(f, delimiter=';')
         for l in reader:
