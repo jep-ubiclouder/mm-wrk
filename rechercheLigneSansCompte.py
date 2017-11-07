0d46745cb666bff7e39cd7d56f6e9bd38ceb52c8
jep-ubiclouder <jean-eric.preis@ubiclouder.com>
2017-11-07 12:31:16 +0100

    dry run

--- rechercheLigneSansCompte.py
+++ rechercheLigneSansCompte.py
@@ -27,7 +27,7 @@ def process():
     sf = Salesforce(username='projets@homme-de-fer.com', password='ubiclouder$2017', security_token='mQ8aTUVjtfoghbJSsZFhQqzJk')
     qry  ='SELECT Bon_de_livraison__c,Code_Client_EURODEP__c,Code_EAN_EURODEP__c,Compte__c,C_A_Brut__c,C_A_Net__c,Date_de_commande__c,Facture__c,Ligne__c,Prix_Brut__c,Prix_Net__c,Produit__c,Quantite__c,Reference_Client__c FROM Commande__c where compte__c = null and Code_Client_EURODEP__c = null  order by Date_de_commande__c desc'
     result = sf.query_all(qry)['records']
-    unknownCompteByFacture = [] 
+    unknownCompteByFacture = {} 
     
     
     # Je mets en relation les facture__c et les id SF
