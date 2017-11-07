5c3aff98c880856c0a3b71d5e65708e041ec4085
jep-ubiclouder <jean-eric.preis@ubiclouder.com>
2017-11-07 11:45:27 +0100

    test5

--- rechercheLigneSansCompte.py
+++ rechercheLigneSansCompte.py
@@ -26,13 +26,33 @@ import csv
 def process():
     sf = Salesforce(username='projets@homme-de-fer.com', password='ubiclouder$2017', security_token='mQ8aTUVjtfoghbJSsZFhQqzJk')
     qry  ='SELECT Bon_de_livraison__c,Code_Client_EURODEP__c,Code_EAN_EURODEP__c,Compte__c,C_A_Brut__c,C_A_Net__c,Date_de_commande__c,Facture__c,Ligne__c,Prix_Brut__c,Prix_Net__c,Produit__c,Quantite__c,Reference_Client__c FROM Commande__c where compte__c = null and Code_Client_EURODEP__c = null  order by Date_de_commande__c desc'
-    ## result = sf.query_all(qry)['records']
+    result = sf.query_all(qry)['records']
+    unknownCompteBySORIFA = [] 
     
-    with open('./venteshisto.csv','r') as f:
+    for r in result:
+        if r['Facture__c'] not in unknownCompteBySORIFA.keys():
+            unknownCompteBySORIFA[r['Facture__c']] = []
         
+        unknownCompteBySORIFA[r['Facture__c']].append(r['Id'])
+        
+    allSorifa =[]
+    byFacture ={}
+    with open('./venteshisto.csv','r') as f:
         reader = csv.DictReader(f, delimiter=';')
         for l in reader:
-            buf = l
-        print( buf)
+            if l['numero document'] not in byFacture.keys():
+                byFacture[l['numero document']] = l['Code client sorifa']
+            
+            if l['Code client sorifa'] not in allSorifa:
+                allSorifa.append(l['Code client sorifa'])
+    
+    qryFindFromSorifa = 'select id,Code_Client_SOFIRA__c from Account where Code_Client_SOFIRA__c in ('+','.join(allSorifa)+')'
+    csrSorifa = sf.query_all(qryFindFromSorifa)['records']
+    bySorifa = {}
+    for r in csrSorifa:
+        bySorifa[r['Code_Client_SOFIRA__c']]=r['Id']
+        
+    
+    print(allSorifa)
 if __name__ == '__main__':
     process()
