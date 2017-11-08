6f2cb1080098ce9a27c4e7ad9d68837fddf64518
jep-ubiclouder <jean-eric.preis@ubiclouder.com>
2017-11-08 13:49:19 +0100

    testing

--- connectProducts.py
+++ connectProducts.py
@@ -14,6 +14,7 @@ from simple_salesforce import (
 import sys
 from _datetime import timedelta
 from datetime import date
+from pickle import FALSE
 
 def processFile():
     sf = Salesforce(username='projets@homme-de-fer.com', password='ubiclouder$2017', security_token='mQ8aTUVjtfoghbJSsZFhQqzJk')
@@ -43,7 +44,50 @@ def processFile():
     
     qryFindProduits =' select id, ProductCode from Product2 where ProductCode in ('+  ','.join(["\'%s\'" % c for c in allProducts])+')'
     
+    allAccountIds = sf.query_all(qryFindFromSorifa)['records']
+    allProductIds = sf.query_all(qryFindProduits)['records'] 
     
-       
+    
+    dictAccounts = {}
+    dictProds ={}
+    for r in allAccountIds:
+        if r['Code_Client_SOFIRA__c'] not in dictAccounts.keys():
+            dictAccounts[r['Code_Client_SOFIRA__c']] = r['Id']
+    for r in allProductIds:
+        if r['ProductCode'] not in dictProds.keys():
+            dictProds[r['ProductCode']] = r['Id']   
+    
+    Insertions = []
+    cpt = 0        
+    with('./complementfev2017.csv','r') as c:
+        reader = csv.DictReader(f, delimiter=';')
+        for l in reader:
+            cpt += 1
+            okAcc = False
+            okPro = False
+            Record = {}
+            Record['Quantite__c'] = l['quantité'] 
+            Record['Facture__c'] = l['numero document']
+            Record['Ligne__c'] =l['ligne document']
+            Record['Prix_Net__c'] = l['prix vente']
+            Record['Prix_Brut__c'] = l['prix vente']
+            # total valeur
+            # '-'.join((r['DATFAC'][:4],r['DATFAC'][4:6],r['DATFAC'][6:]))
+            dwrk = l['date mouvement']
+            Record['Date_de_commande__c'] = '-'.join((r['dwrk'][:4],r['dwrk'][4:6],r['dwrk'][6:]))
+            
+            if l['code article'] in dictProds.keys():
+                Record['Produit__c'] = dictProds[l['code article']]
+                okPro = True
+            if l['Code client sorifa'] in dictAccounts.keys():
+                Record['Compte__c'] = dictAccounts[l['Code client sorifa']]
+                okAcc = True
+            
+            if okPro and okAcc:
+                Insertions.append(Record)
+            else:
+                print('anomalie',l['code article'],[l['Code client sorifa'])
+    print(Insertions)
+    print('Taille insertion',len(Insertions),'lignes ds fichier',cpt)
 if __name__ == '__main__':
     processFile()
