a468aedd71f1d92edf454badc77dcad6d595e9fd
jep-ubiclouder <jean-eric.preis@ubiclouder.com>
2017-11-09 15:50:55 +0100

    final

--- rechercheLigneSansCompte.py
+++ rechercheLigneSansCompte.py
@@ -207,7 +207,7 @@ def ventesInternet():
     
     
     
-    ## r = sf.bulk.Commande__c.insert(readyToUpdate)
+    r = sf.bulk.Commande__c.insert(readyToUpdate)
       
 if __name__ == '__main__':
     ## process()
