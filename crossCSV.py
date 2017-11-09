e3812bd0f8d888323baa8799eac6cc066bacf00a
jep-ubiclouder <jean-eric.preis@ubiclouder.com>
2017-11-09 11:15:40 +0100

    tabs

--- crossCSV.py
+++ crossCSV.py
@@ -6,8 +6,8 @@ leadbyHDF ={}
 
 with open('./leadsAconv.csv','r') as leads:
     reader = csv.DictReader(leads, delimiter=';')
-        for l in reader:
-            leadbyHDF[l['HDF']]  =  l['nom']
+    for l in reader:
+        leadbyHDF[l['HDF']]  =  l['nom']
 prnit(len(leadbyHDF))
 with open('./venteshisto.csv','r') as ventes:
     reader = csv.DictReader(ventes, delimiter=';') 
