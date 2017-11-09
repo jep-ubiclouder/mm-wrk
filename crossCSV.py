91a144156ecc2a851e9b151c73fad7f5bacd4686
jep-ubiclouder <jean-eric.preis@ubiclouder.com>
2017-11-09 11:13:57 +0100

    test

--- crossCSV.py
+++ crossCSV.py
@@ -1,1 +1,16 @@
 import sys
+import csv 
+
+
+leadbyHDF ={}
+
+with open('./leadsAconv.csv','r') as leads:
+    reader = csv.DictReader(leads, delimiter=';')
+        for l in reader:
+            leadbyHDF[l['HDF']]  =  l['nom']
+
+with open('./venteshisto.csv','r') as ventes:
+    reader = csv.DictReader(ventes, delimiter=';') 
+    for l in reader:
+        if l['Code client sorifa'] in leadbyHDF.keys():
+            print(l)
