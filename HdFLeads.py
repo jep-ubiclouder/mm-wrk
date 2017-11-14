cd7ebb33f6589d4f77a90a100ddd0d2f12c090f4
jep-ubiclouder <jean-eric.preis@ubiclouder.com>
2017-11-14 14:40:51 +0100

    test

--- HdFLeads.py
+++ HdFLeads.py
@@ -33,7 +33,8 @@ if __name__ == "__main__":
         for l in reader:
             cpt +=1
             for elem in l:
-                print(elem,l[elem])
+                if len(l[elem])>0:
+                    print(elem,l[elem])
             # 'Categorie_de_client__c'='a020Y000002lgVTQAY''
             #'Tarif'= 'a030Y000003HzI2QAK'
             # 'Reglement' = 'a050Y000000kCUPQA2'
