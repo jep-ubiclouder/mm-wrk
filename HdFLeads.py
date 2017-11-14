fb76ccdfe57151c4d7ecef6375076b16e328f1a8
jep-ubiclouder <jean-eric.preis@ubiclouder.com>
2017-11-14 14:03:18 +0100

    hop

--- HdFLeads.py
+++ HdFLeads.py
@@ -4,9 +4,14 @@ import sys
 
 
 if __name__ == "__main__":
-  pass
-  dom1 = parse('./schema.xml')
-  lines = dom1.getElementsByTagName("mapperTableEntries")
-  for l in lines:
-   print(l.getAttribute('name'),'=>',l.getAttribute('expression'))   
-   
+    pass
+    dom1 = parse('./schema.xml')
+    lines = dom1.getElementsByTagName("mapperTableEntries")
+    for l in lines:
+        print(l.getAttribute('name'),'=>',l.getAttribute('expression'))
+    cpt = 0   
+    with open('./finalImport.csv','r') as f: # Internet2017.csv venteshisto.csv
+        reader = csv.DictReader(f, delimiter=';')
+        for l in reader:
+            cpt +=1 
+    print(cpt)
