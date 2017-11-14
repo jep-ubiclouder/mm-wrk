f2cca4bded317a3ff50521fcac190b254fb399be
jep-ubiclouder <jean-eric.preis@ubiclouder.com>
2017-11-14 14:04:37 +0100

    hop

--- HdFLeads.py
+++ HdFLeads.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     dom1 = parse('./schema.xml')
     lines = dom1.getElementsByTagName("mapperTableEntries")
     for l in lines:
-        print(l.getAttribute('name'),'=>',l.getAttribute('expression'))
+        print(l.getAttribute('name'),'=>',l.getAttribute('expression').split('.')[1])
     cpt = 0   
     with open('./finalImport.csv','r') as f: # Internet2017.csv venteshisto.csv
         reader = csv.DictReader(f, delimiter=';')
