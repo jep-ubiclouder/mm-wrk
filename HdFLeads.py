75bc593f49a870d0dbdee1538169e08f1029bf7d
jep-ubiclouder <jean-eric.preis@ubiclouder.com>
2017-11-14 14:08:39 +0100

    hello

--- HdFLeads.py
+++ HdFLeads.py
@@ -7,12 +7,14 @@ if __name__ == "__main__":
     pass
     dom1 = parse('./schema.xml')
     lines = dom1.getElementsByTagName("mapperTableEntries")
+    mapChamps = {}
     for l in lines:
         if len(l.getAttribute('expression'))> 3 :
-            print('source', l.getAttribute('expression'))
+            # print('source', l.getAttribute('expression'))
             source = l.getAttribute('expression').split('.')[1]
-            dest = l.getAttribute('name') 
-        print(source ,'=>', dest)
+            dest = l.getAttribute('name')
+            mapChamps[source] = dest 
+            print(source ,'=>', dest)
     cpt = 0   
     with open('./finalImport.csv','r') as f: # Internet2017.csv venteshisto.csv
         reader = csv.DictReader(f, delimiter=';')
