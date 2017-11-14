1293cdfd0063e3d351e82a6a3cf5d0299690fefa
jep-ubiclouder <jean-eric.preis@ubiclouder.com>
2017-11-14 14:57:32 +0100

    heop

--- HdFLeads.py
+++ HdFLeads.py
@@ -39,7 +39,7 @@ if __name__ == "__main__":
                     # print(elem,l[elem],mapChamps[elem])
             if len(rec)> 0:
                 rec['Categorie_de_client__c']='a020Y000002lgVTQAY'
-                rec['Tarif']= 'a030Y000003HzI2QAK'']
+                rec['Tarif']= 'a030Y000003HzI2QAK'
                 rec['Reglement'] = 'a050Y000000kCUPQA2'
             #'Tarif'= 'a030Y000003HzI2QAK'
             # 'Reglement' = 'a050Y000000kCUPQA2'
