026e4974617b18de11df246a8f14984a9810cb3c
jep-ubiclouder <jean-eric.preis@ubiclouder.com>
2017-11-14 14:34:36 +0100

    hello

--- HdFLeads.py
+++ HdFLeads.py
@@ -1,7 +1,8 @@
 from  xml.dom.minidom import parse,parseString
 import sys
 import csv
-
+from simple_salesforce import (Salesforce, SalesforceAPI, SFType, SalesforceError, SalesforceMoreThanOneRecord, SalesforceExpiredSession, SalesforceRefusedRequest,
+    SalesforceResourceNotFound, SalesforceGeneralError, SalesforceMalformedRequest)
 
 if __name__ == "__main__":
     pass
