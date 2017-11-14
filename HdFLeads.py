f2ec563448e412c46df0d2ae85796ef391515538
jep-ubiclouder <jean-eric.preis@ubiclouder.com>
2017-11-14 14:57:05 +0100

    dry run

--- HdFLeads.py
+++ HdFLeads.py
@@ -32,12 +32,18 @@ if __name__ == "__main__":
         reader = csv.DictReader(f, delimiter=';')
         for l in reader:
             cpt +=1
+            rec = {}
             for elem in l :
                 if len(l[elem])>0 and elem in mapChamps.keys():
-                    print(elem,l[elem],mapChamps[elem])
-            # 'Categorie_de_client__c'='a020Y000002lgVTQAY''
+                    rec[mapChamps[elem]] = l[elem]
+                    # print(elem,l[elem],mapChamps[elem])
+            if len(rec)> 0:
+                rec['Categorie_de_client__c']='a020Y000002lgVTQAY'
+                rec['Tarif']= 'a030Y000003HzI2QAK'']
+                rec['Reglement'] = 'a050Y000000kCUPQA2'
             #'Tarif'= 'a030Y000003HzI2QAK'
             # 'Reglement' = 'a050Y000000kCUPQA2'
-            if cpt > 2:
+            if cpt > 5:
+                print(rec)
                 sys.exit()
     print(cpt)
