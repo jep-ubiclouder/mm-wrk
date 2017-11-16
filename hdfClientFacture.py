0927938770e6a7139838c2d48f915fec75ac8fc0
jep-ubiclouder <jean-eric.preis@ubiclouder.com>
2017-11-16 10:26:11 +0100

    boucles

--- hdfClientFacture.py
+++ hdfClientFacture.py
@@ -18,18 +18,36 @@ import csv
 
 def process():
     sf = Salesforce(username='projets@homme-de-fer.com', password='ubiclouder$2017', security_token='mQ8aTUVjtfoghbJSsZFhQqzJk')
-    allSorifa =[]
+    
+    mapSorifaIds={}
     with open('./ClentFactures.csv','r') as f: # Internet2017.csv venteshisto.csv
-       
+        allSorifa =[]
         reader = csv.DictReader(f, delimiter=';')
+        cpt =0
         for l in reader:
             ## print(l)
-            if l['Sorifa'] not in allSorifa:
+            if l['Sorifa'] not in mapSorifaIds.keys():
                 allSorifa.append(l['Sorifa'])
-            if l['Parent']  not in allSorifa:
+            if l['Parent']  not in mapSorifaIds.keys():
                 allSorifa.append(l['Parent'])
-    qry = 'select id,Code_Client_SOFIRA__c,Name from Account where Code_Client_SOFIRA__c in ('+','.join(["\'%s\'" % c for c in allSorifa])+')'
-    ## print(qry)
+            cpt +=1
+            if cpt> 800:
+                qry = 'select id,Code_Client_SOFIRA__c,Name from Account where Code_Client_SOFIRA__c in ('+','.join(["\'%s\'" % c for c in allSorifa])+')'
+                
+                allBysorifa =  sf.query_all(qry)['records']
+                for r in allBysorifa:
+                    if r['Code_Client_SOFIRA__c'] not in mapSorifaIds.keys():
+                        mapSorifaIds[r['Code_Client_SOFIRA__c']] = r['Id']
+                cpt= 0
+                 allSorifa =[]
+        
+        if len(allSorifa)>0:         
+            qry = 'select id,Code_Client_SOFIRA__c,Name from Account where Code_Client_SOFIRA__c in ('+','.join(["\'%s\'" % c for c in allSorifa])+')'
+            
+            allBysorifa =  sf.query_all(qry)['records']
+            for r in allBysorifa:
+                if r['Code_Client_SOFIRA__c'] not in mapSorifaIds.keys():
+                    mapSorifaIds[r['Code_Client_SOFIRA__c']] = r['Id']
     ## allBysorifa =  sf.query_all(qry)
     print(len(allSorifa))
 if __name__== '__main__':
