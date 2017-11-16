d17ab498d98af1653fd52b1744400ee190be476c
jep-ubiclouder <jean-eric.preis@ubiclouder.com>
2017-11-16 10:11:11 +0100

    ?

--- hdfClientFacture.py
+++ hdfClientFacture.py
@@ -18,9 +18,9 @@ import csv
 
 def process():
     sf = Salesforce(username='projets@homme-de-fer.com', password='ubiclouder$2017', security_token='mQ8aTUVjtfoghbJSsZFhQqzJk')
-    
+    allSorifa =[]
     with open('./ClentFactures.csv','r') as f: # Internet2017.csv venteshisto.csv
-        allSorifa =[]
+       
         reader = csv.DictReader(f, delimiter=';')
         for l in reader:
             ## print(l)
@@ -29,7 +29,8 @@ def process():
             if l['Parent']  not in allSorifa:
                 allSorifa.append(l['Parent'])
     qry = 'select id,Code_Client_SOFIRA__c,Name from Account where Code_Client_SOFIRA__c in ('+','.join(["\'%s\'" % c for c in allSorifa])+')'
-    allBysorifa =  sf.query_all(qry)
+    print(qry)
+    ## allBysorifa =  sf.query_all(qry)
     print(len(allBysorifa['records']),len(allSorifa))
 if __name__== '__main__':
     process()
