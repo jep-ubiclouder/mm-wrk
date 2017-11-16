64f8271eddf318148503f9445fe1193a452137c2
jep-ubiclouder <jean-eric.preis@ubiclouder.com>
2017-11-16 09:57:32 +0100

    first

--- hdfClientFacture.py
+++ hdfClientFacture.py
@@ -1,1 +1,26 @@
+from simple_salesforce import (
+    Salesforce,
+    SalesforceAPI,
+    SFType,
+    SalesforceError,
+    SalesforceMoreThanOneRecord,
+    SalesforceExpiredSession,
+    SalesforceRefusedRequest,
+    SalesforceResourceNotFound,
+    SalesforceGeneralError,
+    SalesforceMalformedRequest
+)
+ 
 import sys
+from _datetime import timedelta
+import datetime
+import csv
+
+def process():
+    with open('./ClentFactures.csv','r') as f: # Internet2017.csv venteshisto.csv
+        reader = csv.DictReader(f, delimiter=';')
+        for l in reader:
+            print(l)
+
+if __name__== '__main__':
+    process()
