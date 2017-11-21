1bdb082ac6f997cd19eb9f9e1db789ea2134ecba
jep-ubiclouder <jean-eric.preis@ubiclouder.com>
2017-11-21 14:25:52 +0100

    plop

--- crossCSV.py
+++ crossCSV.py
@@ -1,17 +1,21 @@
 import sys
 import csv 
+from simple_salesforce import (Salesforce, SalesforceAPI, SFType, SalesforceError, SalesforceMoreThanOneRecord, SalesforceExpiredSession, SalesforceRefusedRequest,
+    SalesforceResourceNotFound, SalesforceGeneralError, SalesforceMalformedRequest)
+
+def process():
+    sf = Salesforce(username='projets@homme-de-fer.com', password='ubiclouder$2017', security_token='mQ8aTUVjtfoghbJSsZFhQqzJk')
+    
+    allProduits = []
+    with open('./Fev2017.csv','r') as f: # Internet2017.csv venteshisto.csv
+        allSorifa =[]
+        reader = csv.DictReader(f, delimiter=';')
+        for l in reader:
+            if l['article'] not in allProduits:
+                allProduits.append(l['Article'])
+
+    print(allProduits,len(allProduits))
+if __name__ == '__main__':
+    process()
 
 
-leadbyHDF ={}
-
-with open('./leadsAconv.csv','r') as leads:
-    reader = csv.DictReader(leads, delimiter=';')
-    for l in reader:
-        leadbyHDF[l['HDF']]  =  l['nom']
-print(len(leadbyHDF))
-with open('./venteshisto.csv','r') as ventes:
-    reader = csv.DictReader(ventes, delimiter=';') 
-    for l in reader:
-        if l['Code client sorifa'] in leadbyHDF.keys():
-            print("%s;%s;%s;%s;%s;%s"%(l['Code client sorifa'],leadbyHDF[l['Code client sorifa']],l['date mouvement'],l['quantité'],l['code article'],l['prix vente']))
-            
