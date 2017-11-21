d58fc8a4a7985d1e4199642c0e873799fcd7e086
jep-ubiclouder <jean-eric.preis@ubiclouder.com>
2017-11-21 14:45:29 +0100

    comptes et produits

--- crossCSV.py
+++ crossCSV.py
@@ -7,19 +7,27 @@ def process():
     sf = Salesforce(username='projets@homme-de-fer.com', password='ubiclouder$2017', security_token='mQ8aTUVjtfoghbJSsZFhQqzJk')
     
     allProduits = []
+    allComptes =[]
     with open('./Fev2017.csv','r') as f: # Internet2017.csv venteshisto.csv
         allSorifa =[]
         reader = csv.DictReader(f, delimiter=';')
         for l in reader:
             if l['Code Ean'] not in allProduits:
                 allProduits.append(l['Code Ean'])
+            if l['code client Eurodep'] not in allComptes:
+                allComptes.append(l['code client Eurodep'])
 
     print(allProduits,len(allProduits))
     qryAllProductBySORIFA = 'select id,EAN__c,Name from Product2 where EAN__c in ('+','.join(["\'%s\'" % c for c in allProduits])+')'
     
     print(qryAllProductBySORIFA)
-    recs = sf.query_all(qryAllProductBySORIFA)['records']
-    print(recs,len(recs))
+    recsP = sf.query_all(qryAllProductBySORIFA)['records']
+    print(len(recsP))
+    
+    qryAllComptes = 'select id,Code_EURODEP__c,Name from Account where Code_EURODEP__c in ('+','.join(["\'%s\'" % c for c in allProduits])+')'
+    recsA = sf.query_all(qryAllComptes)['records']
+    print(len(recsA))
+    
 if __name__ == '__main__':
     process()
 
