e0921034c4a73597cb9f8f9c223c2b279a495bc9
jep-ubiclouder <jean-eric.preis@ubiclouder.com>
2018-02-26 11:15:48 +0100

    test

--- AssemblyDumpPro.py
+++ AssemblyDumpPro.py
@@ -12,6 +12,8 @@ def getCredentials(isTest):
     import json
     cipher = Fernet(clef)
     creds = json.loads(cipher.decrypt(data).decode())
+    creds = json.loads(cipher.decrypt(data).decode())
+    creds =  {'user':'hp@maisonmoderne.lu', 'passwd':'Ubiclouder$2018','security_token':'ZI8DAzQdqx3ElEhliEarsEV5'}
     return creds
 
 
